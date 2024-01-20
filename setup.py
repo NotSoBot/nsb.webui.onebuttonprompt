2df758af18037e7290b99429e82e5059eb86253d
Dan <31395415+cakedan@users.noreply.github.com>
2024-01-19 21:51:27 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup_kwargs = {
+    'name': 'OneButtonPrompt',
+    'version': '0.0.1',
+    'url': 'https://github.com/notsobot/nsb.webui.onebuttonprompt',
+    'author': 'AIrjen',
+    'description': (
+        'Generate prompts',
+    ),
+    'packages': find_packages(),
+    'include_package_data': True,
+    'platforms': 'any',
+}
+
+setup(**setup_kwargs)
+
